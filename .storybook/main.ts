3d869c24a6662a505c1f7703ee425629abf91a54
kjin <kjin.code@gmail.com>
2023-12-22 15:43:07 +0900

    feat: setting emotion css

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
+const path = require("path");
+const toPath = (_path) => path.join(process.cwd(), _path);
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -21,5 +23,29 @@ const config: StorybookConfig = {
     autodocs: "tag",
   },
   staticDirs: ["..\\public"],
+  webpackFinal: async (config) => {
+    if (config.module && config.resolve) {
+      if (config.module?.rules && config.resolve?.extensions) {
+        config.module.rules.push({
+          test: /\.(ts|tsx)$/,
+          loader: require.resolve("babel-loader"),
+          options: {
+            presets: [
+              ["react-app", { flow: false, typescript: true }],
+              require.resolve("@emotion/babel-preset-css-prop"),
+            ],
+          },
+        });
+        config.resolve.extensions.push(".ts", ".tsx");
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "@emotion/styled": toPath("node_modules/@emotion/styled"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
+        };
+      }
+    }
+    return config;
+  },
 };
 export default config;
