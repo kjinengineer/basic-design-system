8ecbab6cb5610864b684b1458acbfdb5eb102e5e
kjin <kjin.code@gmail.com>
2023-12-24 15:28:15 +0900

    feat: change shadow component css

--- src/stories/components/shadow/Shadow.tsx
+++ src/stories/components/shadow/Shadow.tsx
@@ -5,29 +5,19 @@ interface Props {
 
 export const Shadow = ({ type, colored }: Props) => {
   const color = colored ? "#0D689E20" : "rgb(0, 0, 0, 0.1)";
-  if (type === "Large") {
-    return (
-      <div
-        style={{
-          width: "120px",
-          height: "120px",
-          backgroundColor: "white",
-          borderRadius: "15px",
-          boxShadow: `0 4px 12px ${color}, 0 2px 4px ${color}`,
-        }}
-      ></div>
-    );
-  } else {
-    return (
-      <div
-        style={{
-          width: "120px",
-          height: "120px",
-          backgroundColor: "white",
-          borderRadius: "15px",
-          boxShadow: `0 2px 4px ${color}, 0 1px 2px ${color}`,
-        }}
-      ></div>
-    );
-  }
+
+  return (
+    <div
+      style={{
+        width: "120px",
+        height: "120px",
+        backgroundColor: "white",
+        borderRadius: "15px",
+        boxShadow:
+          type === "Large"
+            ? `0 4px 12px ${color}, 0 2px 4px ${color}`
+            : `0 2px 4px ${color}, 0 1px 2px ${color}`,
+      }}
+    ></div>
+  );
 };
