285ec378b26b6155a6cb12fc756d352a4b136966
kjin <kjin.code@gmail.com>
2023-12-25 15:45:43 +0900

    feat: update Storybook title and logo

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -2,6 +2,6 @@ import { create } from "@storybook/theming/create";
 
 export default create({
   base: "light",
-  brandTitle: "BASIC SYSTEM",
+  brandTitle: "BASIC DESIGN SYSTEM",
   brandTarget: "_self",
 });
